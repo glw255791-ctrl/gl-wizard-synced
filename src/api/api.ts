9c69740e1a0ad3aac1b2d4eb5760d013fcb923f5
dobrosav22 <dobrosav.djukic@gmail.com>
2025-05-24 17:44:18 +0200

    remove supabase

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,34 +1,34 @@
-import { createClient } from "@supabase/supabase-js";
+// import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL || "";
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || "";
-const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY || "";
+// const supabaseUrl = process.env.SUPABASE_URL || "";
+// const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || "";
+// const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY || "";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceRoleKey);
+// export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceRoleKey);
 
-export const handleRegister = async (
-  userId: string,
-  password: string,
-  firstName: string,
-  lastName: string
-): Promise<{ success: true }> => {
-  if (!userId) throw new Error("User is not defined");
+// export const handleRegister = async (
+//   userId: string,
+//   password: string,
+//   firstName: string,
+//   lastName: string
+// ): Promise<{ success: true }> => {
+//   if (!userId) throw new Error("User is not defined");
 
-  const { error: pwError } = await supabase.auth.updateUser({ password });
-  if (pwError) throw new Error("Failed to set password: " + pwError.message);
+//   const { error: pwError } = await supabase.auth.updateUser({ password });
+//   if (pwError) throw new Error("Failed to set password: " + pwError.message);
 
-  const { error: profileError } = await supabase
-    .from("profiles")
-    .update({
-      first_name: firstName,
-      last_name: lastName,
-    })
-    .eq("id", userId);
+//   const { error: profileError } = await supabase
+//     .from("profiles")
+//     .update({
+//       first_name: firstName,
+//       last_name: lastName,
+//     })
+//     .eq("id", userId);
 
-  if (profileError)
-    throw new Error("Failed to update profile: " + profileError.message);
+//   if (profileError)
+//     throw new Error("Failed to update profile: " + profileError.message);
 
-  return { success: true };
-};
+//   return { success: true };
+// };
