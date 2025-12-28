77fcbee643b2dc98dac083b3d790414ce1e6892e
dobrosav22 <dobrosav.djukic@gmail.com>
2025-12-28 19:17:37 +0100

    next update

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,6 @@ export default function RootPage() {
   return (
     <main style={{ padding: "4rem", textAlign: "center" }}>
       <h1>Redirecting to Login...</h1>
-      <p>
-        If not redirected, <a href="/login">click here</a>.
-      </p>
     </main>
   );
 }
